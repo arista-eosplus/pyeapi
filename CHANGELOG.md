42532151c8911f0d90e805ffea61abba696df7f8
Peter Sprygada <sprygada@arista.com>
2015-04-07 18:35:14 -0400

    updates project files to support additional library
    This commit updates the supporting files to add netaddr to the dependency
    list for pyeapi.  After this patch is applied, netaddr will be a required
    library for all implementations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ Python Client for eAPI
 
 ## v0.3.0, IN PROGRESS
 
+- fixes an issue with configuring stp portfast edge correctly
+- added initial support for system api module
+- added initial support for acl api module (standard)
+- added initial api support for mlag configuration
 - added tag feature to eapi.conf 
 
 ## v0.2.1, 03/28/2015
--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=[],
+    install_requires=['netaddr'],
 
     # List additional dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
