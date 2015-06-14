6c11356447425a7c529d8e7aada7922ec0140106
Peter Sprygada <sprygada@arista.com>
2015-06-14 08:52:28 -0400

    updated version information to 0.3.1

--- pyeapi/__init__.py
+++ pyeapi/__init__.py
@@ -29,7 +29,7 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Arista EOS+'
 
 
