d90fc67bd2c23358cd1fed3404195b515fb3ad26
Philip DiLeo <phil@arista.com>
2015-09-16 12:34:19 -0400

    Fix unit test

--- test/unit/test_api_bgp.py
+++ test/unit/test_api_bgp.py
@@ -98,15 +98,26 @@ class TestApiBgp(EapiConfigUnitTest):
                 func = function('set_router_id', rid, True)
             self.eapi_positive_config_test(func, cmds)
 
-    def test_maximum_paths(self):
+    def test_maximum_paths_just_max_path(self):
+        for state in ['config', 'default']:
+            max_paths = 20
+            if state == 'config':
+                cmds = ['router bgp 65000', 'maximum-paths 20']
+                func = function('set_maximum_paths', max_paths)
+            elif state == 'default':
+                cmds = ['router bgp 65000', 'default maximum-paths']
+                func = function('set_maximum_paths', default=True)
+            self.eapi_positive_config_test(func, cmds)
+
+    def test_maximum_paths_max_path_and_ecmp(self):
         for state in ['config', 'default']:
             max_paths = 20
             max_ecmp_path = 20
             if state == 'config':
-                cmds = ['router bgp 65000']
+                cmds = ['router bgp 65000', 'maximum-paths 20 ecmp 20']
                 func = function('set_maximum_paths', max_paths, max_ecmp_path)
             elif state == 'default':
-                cmds = ['router bgp 65000']
+                cmds = ['router bgp 65000', 'default maximum-paths']
                 func = function('set_maximum_paths', default=True)
             self.eapi_positive_config_test(func, cmds)
 
