2d14de25c73bcd02e52c1473578f44e179ddbf90
Andrii Rykhlivskyi <andrii@arista.com>
2020-01-27 19:16:26 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.8',
     ],
 
     keywords='networking arista eos eapi',
